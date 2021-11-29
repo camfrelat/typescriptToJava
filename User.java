7c7b8d431bd2be55720786382aa2f72091b0e73a
camille <camfrelat@gmail.com>
2021-11-29 12:50:21 +0100

    finished quest

---
+++ User.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+import addresses.Address;
+import photos.Photo;
+
+public class User {
+    private Date birthdate;
+    private Number size;
+    private Photo photos;
+    private Address address;
+
+    public Date getBirthdate() {return birthdate;}
+    public void setBirthdate(Date birthdate) {this.birthdate = birthdate;}
+
+    public Number getSize() {return size;}
+    public void setSize(Number size) {this.size = size;}
+
+    public Photo getPhotos() {return photos;}
+    public void setPhotos(Photo photos) {this.photos = photos;}
+
+    public Address getAddress() {return address;}
+    public void setAddress(Address address) {this.address = address;}
+}
---
+++ photos/Photo.java
@@ -0,0 +1,20 @@
+package photos;
+
+public class Photo {
+    private String url;
+    private String filename;
+    private Number width;
+    private Number height;
+
+    public String getUrl() {return url;}
+    public void setUrl(String url) {this.url = url;}
+
+    public String getFilename() {return filename;}
+    public void setFilename(String filename) {this.filename = filename;}
+
+    public Number getwidth() {return width;}
+    public void setWidth(Number width) {this.width = width;}
+
+    public Number getHeight() {return height;}
+    public void setHeight(Number height) {this.height = height;}
+}
